4fd9b53ccd80dec493e9887d2a7d671589bc9e02
Teklis Benzi <teklisb@gmail.com>
2025-08-06 19:03:15 +0530

    Refactor multiplexing logic and update seven-segment display patterns. Finnaly it works!

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,8 @@
 uint8_t counter = 0;
 unsigned long counter_last_millis = 0;
 
+uint8_t digit_to_be_displayed = 0; // The digit to be displayed on the display
+
 const byte segmentPins[NUM_ANODES] = {A, B, C, D, E, FD, G};
 const byte cathodePins[NUM_CATHODES] = {H, I, J, K, L, M};
 
@@ -43,7 +45,7 @@ const byte cathodePins[NUM_CATHODES] = {H, I, J, K, L, M};
 // standard seven-segment display pattern, but it works!
 // What can I say? It's a hacky solution that works for this specific display.
 const byte seven_segment_lookup[10]{
-    0b0000000, // 0
+    0b0111111, // 0
     0b0000110, // 1
     0b1011011, // 2
     0b1001111, // 3
@@ -55,41 +57,87 @@ const byte seven_segment_lookup[10]{
     0b1101111, // 9
 };
 
-void writeSegments(byte segmentPattern) {
-  for (int i = 0; i < NUM_ANODES; i++) { // Loop only 7 times
-    if ((segmentPattern >> i) & 0x01) {
+void writeSegments(byte segmentPattern)
+{
+  for (int i = 0; i < NUM_ANODES; i++)
+  { // Loop only 7 times
+    if ((segmentPattern >> i) & 0x01)
+    {
       digitalWrite(segmentPins[i],
                    HIGH); // Turn segment ON (assuming common anode, active LOW)
-    } else {
+    }
+    else
+    {
       digitalWrite(segmentPins[i], LOW); // Turn segment OFF
     }
   }
 }
 
-void setup() {
-  for (int i = 0; i < NUM_ANODES; i++) {
+void setup()
+{
+  for (int i = 0; i < NUM_ANODES; i++)
+  {
     pinMode(segmentPins[i], OUTPUT);
   }
-  for (int i = 0; i < NUM_CATHODES; i++) {
+  for (int i = 0; i < NUM_CATHODES; i++)
+  {
     pinMode(cathodePins[i], OUTPUT);
   }
 
   pinMode(K, INPUT_PULLUP);
 }
 
-void loop() {
-  for (int i = 0; i <= 9; i++) {
-    writeSegments(seven_segment_lookup[i]);
-    delay(MULTIPLEX_DELAY);
+void loop()
+{
+  //writeSegments(seven_segment_lookup[counter]);
+
+  if (millis() - counter_last_millis > 1000)
+  {
+    counter++;
+    counter_last_millis = millis();
+    if (counter > 99)
+    {
+      counter = 0;
+    }
   }
 
-  if(millis() - counter_last_millis > 1000){
-      counter++;
-      counter_last_millis = millis();
-      if(counter > 199){
-        counter = 0;
-      }
+  // get digit data. Digit 0 is leftmost
+  uint8_t dig2 = (counter % 10);       // ones
+  uint8_t dig1 = (counter / 10) % 10;  // tens
+  uint8_t dig0 = (counter / 100) % 10; // hundreds
+
+  // Since all the cathodes are connected via transistors (NPN in this case),
+  // we turn them off by pulling the base low.
+  // This done every loop so that there is no ghosting on the display.
+  for (int i = 0; i < NUM_CATHODES; i++)
+  {
+    digitalWrite(cathodePins[i], LOW);
   }
+
+  switch (digit_to_be_displayed)
+  {
+  case 0:
+    writeSegments(seven_segment_lookup[dig0]);
+    digitalWrite(M, HIGH); // Turn on the cathode for digit 0
+    delay(2); // Short delay to allow the display to stabilize
+    break;
+  case 1:
+    writeSegments(seven_segment_lookup[dig1]);
+    digitalWrite(L, HIGH); // Turn on the cathode for digit 1
+    delay(2); // Short delay to allow the display to stabilize
+    break;
+  case 2:
+    writeSegments(seven_segment_lookup[dig2]);
+    digitalWrite(K, HIGH); // Turn on the cathode for digit 2
+    delay(2); // Short delay to allow the display to stabilize
+    break;
+  }
+  digit_to_be_displayed++; // Increment the digit to be displayed
+  // Reset to 0 if it exceeds the number of digits available on the display
+  if (digit_to_be_displayed >= 3) digit_to_be_displayed = 0;
+
+  
+
   // writeSegments(seven_segment_lookup[counter]);
 
   // static uint16_t btndbc = 0;
@@ -102,5 +150,6 @@ void loop() {
   //     counter = 0;
   //   }
   // }
-  // delay(1);
+  delay(analogRead(A1)); // Delay based on the analog reading from A1
+  // This is just to visually see the display multiplexing effect.
 }
